85901a77cc700f2de59ce1f694cce5330a0b37e5
olegstasiw <olegstasiw@icloud.com>
2020-12-30 14:42:58 +0200

    Initial commit

---
+++ Countries-Info SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countries-Info SwiftUI
+//
+//  Created by Oleh Stasiv on 30.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
